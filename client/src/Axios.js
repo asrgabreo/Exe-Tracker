8eb2ff7e070540bb1cf3b607157d8dff409d43a5
asrgabreo <anshuasr10@gmail.com>
2021-07-03 19:28:19 +0530

    deploy setup2

--- client/src/Axios.js
+++ client/src/Axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+let API_ROOT = 'http://localhost:5000';
+
+if(process.env.NODE_ENV === 'production'){
+  API_ROOT = 'https://exe-tracker.herokuapp.com'
+}
+
+
 const Axios = axios.create({
-  baseURL: 'https://exe-tracker.herokuapp.com'
+  baseURL: API_ROOT
 });
 
 export default Axios;
