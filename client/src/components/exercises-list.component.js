d7d37fc7d1e06e489a4fffdee788dd8798ef3221
asrgabreo <anshuasr10@gmail.com>
2021-07-03 17:16:33 +0530

    improved setup

---
+++ client/src/components/exercises-list.component.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+let API_ROOT = 'http://localhost:8000';
+
+if(process.env.NODE_ENV === 'production'){
+  API_ROOT = 'https://seminar-eval.herokuapp.com';
+}
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+    </td>
+  </tr>
+)
+
+export default class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this)
+
+    this.state = {exercises: []};
+  }
+
+  componentDidMount() {
+    axios.get(`${API_ROOT}/exercises/`)
+      .then(response => {
+        this.setState({ exercises: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteExercise(id) {
+    axios.delete(`${API_ROOT}/exercises/`+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    })
+  }
+
+  exerciseList() {
+    return this.state.exercises.map(currentexercise => {
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.exerciseList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
--- server.js
+++ server.js
@@ -6,7 +6,15 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+const morgan = require('morgan');
+app.use(morgan("tiny"));
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.use(cors());
 app.use(express.json());
 
